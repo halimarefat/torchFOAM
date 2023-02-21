c73bdec9c8c1a2157d21760ac1e9eac677f996e7
halimarefat <halimarefat@gmail.com>
2023-02-21 18:07:06 +0000

    -lscotch is deleted from options

--- torchFOAM.C
+++ torchFOAM.C
@@ -2,8 +2,11 @@
 #include <iostream>
 
 int main() {
-  std::cout << "PyTorch version: "
+  /*
+    std::cout << "PyTorch version: "
     << TORCH_VERSION_MAJOR << "."
     << TORCH_VERSION_MINOR << "."
     << TORCH_VERSION_PATCH << std::endl;
+    */
+    return 0;
 }
