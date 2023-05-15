fdcc503c5e6ea075d651735e75525de369ade55a
halimarefat <halimarefat@gmail.com>
2023-05-14 23:36:41 -0400

    still developing PyNNTraining

--- PyNNTraining/training.py
+++ PyNNTraining/training.py
@@ -1,14 +1,68 @@
 import torch
+import tqdm
 import numpy as np
 from model import nnModel
 
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, path):
+        self.data = np.loadtxt(path) 
 
-data = np.loadtxt('/home/hmarefat/scratch/torchFOAM/datasetGen/dataset_460_490.npy')
+    def __len__(self):
+        return len(self.data)
 
-train_loader = torch.utils.data.DataLoader(data, batch_size=1028, shuffle=False)
+    def __getitem__(self, idx):
+        features = self.data[idx][0:8]
+        labels = self.data[idx][9] 
+        sample = {"data": features, "target": labels}
+        return sample
+
+data = CustomDataset('/home/hmarefat/scratch/torchFOAM/datasetGen/dataset_scaled.npy')
+
+print(len(data))
+
+train_val_split = 0.8
+batch_sz = 1028
+device = torch.device("cuda")
+epochs = 500
+train_sz = int(train_val_split * len(data))
+val_sz = len(data) - train_sz
+
+train_ds, val_ds = torch.utils.data.random_split(data, [train_sz, val_sz]) 
+
+train_loader = torch.utils.data.DataLoader(train_ds, batch_size=batch_sz, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_ds, batch_size=batch_sz, shuffle=True)
 
 model = nnModel(inp_s=9, hid_s=63, out_s=1, layer_num=4)
+model.to(device)
 
-torch.jit.load(model, "/home/hmarefat/scratch/torchFOAM/nnTraining/best_model_0510235.pt")
+print('+-- model summary:')
 print(model)
-print(data.shape)
+print('+-- dataset  shape: ', len(data))
+#print('+-- train ds shape: ', train_ds.shape)
+#print('+-- val   ds shape: ', val_ds.shape)
+
+#train_noBatch = train_ds.shape[0] / batch_sz
+#val_noBatch = val_ds.shape[0] / batch_sz
+
+#print('+-- train batchs #: ', train_noBatch)
+#print('+-- val   batchs #: ', val_noBatch)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+train_loss = 0 
+model.train()
+for epoch in range(epochs):
+    for batch in train_loader:
+        
+        train_feat = batch.data.to(device)
+        train_labs = batch.target.to(device)
+        train_pred = model.forward(train_feat)
+        train_loss += torch.nn.functional.mse_loss(train_pred, train_labs)
+
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+    train_loss /= 1028 
+
+#torch.jit.load(model, "/home/hmarefat/scratch/torchFOAM/nnTraining/best_model_0510235.pt")
+
